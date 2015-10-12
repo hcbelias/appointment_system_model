5ca81de5618926b1d0427291489ce126c2c170f0
henrique <hcbe2004@gmail.com>
2015-10-12 13:50:38 -0300

    updating model

--- MiningMath/EntityDataModel.Designer.cs
+++ MiningMath/EntityDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\avenuecode\documents\visual studio 2013\Projects\MiningMath\MiningMath\EntityDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Henrique\Project\appointment_system_model\MiningMath\EntityDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
